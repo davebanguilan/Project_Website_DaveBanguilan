00526e83e20cf49480d6759d105a9c97de0145b3
davebanguilan <desbanguilan@gmail.com>
2021-02-19 00:34:08 +0800

    Hero Done

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import {Container, Button, Typography, Grid} from '@material-ui/core';
+import EmailOutlinedIcon from '@material-ui/icons/EmailOutlined';
+import GetAppOutlinedIcon from '@material-ui/icons/GetAppOutlined';
+
+import useStyles from "./styles";
+import Me from '../../images/Me.jpg';
+
+const Hero = () => {
+    const classes = useStyles();
+    return (
+        <Container className={classes.hero}>
+            <Grid container spacing={3} justify="center" alignItems="center">
+                <Grid item xs={12} sm={8} justify="center">
+                    <Typography variant="h5">
+                        <span variant="h5" className={classes.primaryColor}>Hey there,</span> I'm Dave Banguilan
+                    </Typography>
+                    <Typography className={classes.textColor} variant="h3">
+                        Software Developer
+                    </Typography>
+                    <Typography className={classes.gray} variant="h6">
+                        5th year student 🎓 | Night Owl 🌃
+                    </Typography>
+                    <div className={classes.buttonGroup}>
+                        <Button startIcon={<EmailOutlinedIcon />} variant="outlined" className={classes.button1} size="large">Contact Me</Button>
+                        <Button startIcon={<GetAppOutlinedIcon />} variant="outlined" border={1} className={classes.button2} size="large">Download CV</Button>
+                    </div>
+                </Grid>
+                <Grid item container xs={12} sm={4} justify="flex-end" alignItems="flex-end">
+                    <img className={classes.imgAvatar} src={Me} alt="Dave Banguilan"/>
+                </Grid>
+            </Grid>
+        </Container>
+    )
+}
+
+export default Hero;
---
+++ src/components/Hero/styles.js
@@ -0,0 +1,52 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles((theme) => ({
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+
+    },
+  },
+  hero: {
+    marginTop: "80px",
+    flexGrow: 1,
+  },
+  primaryColor: {
+    color: "#19d3da",
+  },
+  textColor: {
+    color: "#323232",
+  },
+  gray: {
+    color: "#AAAAAA"
+  },
+  imgAvatar: {
+    width: "275px",
+    height: "275px",
+    borderRadius: "50%",
+  },
+  buttonGroup: {
+      marginTop: "50px",
+  },
+  button1: {
+    color: "#FFFFFF",
+    border: '1px solid',
+    backgroundColor: "#19d3da",
+    '&:hover': {
+        color: "#19d3da",
+        backgroundColor: "#FFFFFF"
+    }
+  },
+  button2: {
+    margin: "0 10px",
+    color: "#19d3da",
+    border: '1px solid',
+    backgroundColor: "#FFFFFF",
+    boxShadow: "#00aaaa 0px 0px 10px 0px'",
+    '&:hover': {
+        color: "#FFFFFF",
+        backgroundColor: "#19d3da",
+    }
+},
+
+}));
